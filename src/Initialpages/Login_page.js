ee66ae68b6d3642942ead80023561e0157e24118
Vignesh <bvjamalian@gmail.com>
2025-05-23 10:04:47 +0530

    New on Navigation

--- src/Initialpages/Login_page.js
+++ src/Initialpages/Login_page.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
-const Login_page = () => {
+const Login_page = ({ navigation }) => {
+  const Data = [
+    { "id": 1, "Title": "Login_page" },
+    { "id": 2, "Title": "Home_Page" },
+    { "id": 3, "Title": "Lists" },
+  ]
   return (
-    <View>
-      <Text>Login_page</Text>
+    <View style={styles.Container}>
+      <View style={styles.Innnerdatas}>
+        {Data.map((i) => {
+          return (<TouchableOpacity style={styles.IndividualBoxes} key={i.id} onPress={() => navigation.navigate(i.Title)}>
+            <Text>{i.Title}</Text>
+          </TouchableOpacity>)
+        })}
+      </View>
     </View>
   )
 }
 
 export default Login_page
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1
+  },
+  Innnerdatas: { flex: 1, flexDirection: "row", justifyContent: "space-evenly", marginTop: 10 },
+  IndividualBoxes: { height: 50, minWidth: 120, padding: 12, borderColor: "lightgrey", borderWidth: 2, justifyContent: "center", alignItems: "center", borderRadius: 10 }
+})
--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Text } from 'react-native';
 import Login_page from './Initialpages/Login_page';
 import Home_Page from './Initialpages/Home_Page';
+import Lists from './Initialpages/Lists';
 const Stack = createStackNavigator();
 
 
@@ -11,9 +12,10 @@ const Stack = createStackNavigator();
 function Navigation() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='Login_page' screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName='Login_page' screenOptions={{ headerBackTitle:"text" }}>
                 <Stack.Screen name="Login_page" component={Login_page} />
                 <Stack.Screen name="Home_Page" component={Home_Page} />
+                <Stack.Screen name="Lists" component={Lists} />
             </Stack.Navigator>
         </NavigationContainer>
     );
