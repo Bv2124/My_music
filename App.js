87fbe04f7e3dda4ad445b3d5661328fe980485cd
Vignesh <bvjamalian@gmail.com>
2025-07-08 16:00:59 +0530

    Error while running

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Provider } from 'react-redux';
+import { Provider } from 'redux';
 import store from './src/store';
 import Navigation from './src/Navigation'
 
